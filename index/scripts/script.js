12b92460803b8c3df04c7c2ebb8bad7c85a71bea
vestigeyu <vestigeyu@163.com>
2015-11-29 04:11:25 +0800

    kaisite_3

--- index/scripts/script.js
+++ index/scripts/script.js
@@ -1,3 +1,4 @@
+// 轮播图效果 
 jQuery(".fullSlide").hover(function() {
     jQuery(this).find(".prev,.next").stop(true, true).fadeTo("show", 0.5)
 },
@@ -18,3 +19,34 @@ jQuery(".fullSlide").slide({
         }
     }
 });
+// 轮播图效果 
+
+// 公告通知效果
+(function (win){
+    var callboarTimer;
+    var callboard = $('#callboard');
+    var callboardUl = callboard.find('ul');
+    var callboardLi = callboard.find('li');
+    var liLen = callboard.find('li').length;
+    var initHeight = callboardLi.first().outerHeight(true);
+    win.autoAnimation = function (){
+    if (liLen <= 1) return;
+        var self = arguments.callee;
+        var callboardLiFirst = callboard.find('li').first();
+        callboardLiFirst.animate({
+        marginTop:-initHeight
+        }, 500, function (){
+            clearTimeout(callboarTimer);
+            callboardLiFirst.appendTo(callboardUl).css({marginTop:0});
+            callboarTimer = setTimeout(self, 5000);
+        });
+    }
+    callboard.mouseenter(
+    function (){
+        clearTimeout(callboarTimer);
+    }).mouseleave(function (){
+        callboarTimer = setTimeout(win.autoAnimation, 5000);
+    });
+}(window));
+setTimeout(window.autoAnimation, 5000); 
+// 公告通知效果
